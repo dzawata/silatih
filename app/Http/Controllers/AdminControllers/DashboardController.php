4b7b9cbfce2cdec2bc320dcffb7c39f940de93fd
dzawata <interisty91@gmail.com>
2024-02-12 07:56:45 +0700

    updating count dashboard

--- app/Http/Controllers/AdminControllers/DashboardController.php
+++ app/Http/Controllers/AdminControllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Core\Rekomendasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -18,12 +19,13 @@ class DashboardController extends Controller
     {
 
         $result['judul'] = 'Dashboard';
-        
+
         $result['pengguna'] = DB::table('users')->count();
         $result['pelatihan'] = DB::table('pelatihan')->count();
         $result['sample'] = DB::table('sample')->count();
-        $result['rekomendasi'] = DB::table('rekomendasi')->count();
+        $result['rekomendasi'] = DB::table('rekomendasi')
+            ->join('tenagakerja', 'tenagakerja.nik', 'rekomendasi.nik')
+            ->count();
         return view('admin.dashboard')->with('result', $result);
     }
-
 }
